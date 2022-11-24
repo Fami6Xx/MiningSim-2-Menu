c6f04fd26a09a91d7f3f2101e770f38f9dd52213
Fami6Xx <libojer@gmail.com>
2022-11-24 19:14:59 +0100

    feat: crates & egg opening

--- miningSim2Menu.lua
+++ miningSim2Menu.lua
@@ -22,3 +22,39 @@ end)
 Settings:NewKeybind("Toggle UI", "Toggles UI", Enum.KeyCode.P, function()
 	Library:ToggleUI()
 end)
+
+local Gamble = Window:NewTab("Crates an Eggs")
+
+local Crates = Gamble:NewSection("Crates")
+Crates:NewButton("Open all", "Opens all crates in inventory", function() 
+    local crates = v8("Crates")
+    for i=1,100 do
+        for c in pairs(crates) do
+            game:GetService("ReplicatedStorage").Events.OpenCrate:FireServer(c)
+            wait(0.01)
+        end
+    end
+end)
+
+local Eggs = Gamble:NewSection("Eggs")
+Eggs:NewToggle("Fast Open selected egg", "Sends events so that it is fast", function(state)
+    if state then
+        local selected = getgenv().selectedEgg
+        if not selected then return end
+        getgenv().autoEggs = true
+        while getgenv().autoEggs do
+            game:GetService("ReplicatedStorage").Events.OpenEgg:FireServer(selected, true, true)
+            wait(0.5)
+        end
+    else
+        getgenv().autoEggs = false
+    end
+end)
+
+local object = {}
+local eggsNum = 0
+for i in pairs(v8("Eggs")) do object[eggsNum]=i eggsNum=eggsNum+1 end
+
+local eggDrop = Eggs:NewDropdown("Egg to open", "Selected egg to be opened by fast open", object, function(selected) 
+    getgenv().selectedEgg = selected
+end)
