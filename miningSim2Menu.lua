de7b8d88ce681b0fef44af27680c79b83768353f
Fami6Xx <libojer@gmail.com>
2022-11-24 19:14:13 +0100

    general & settings

--- miningSim2Menu.lua
+++ miningSim2Menu.lua
@@ -4,3 +4,21 @@ local v8 = require(game:GetService("ReplicatedStorage").LoadModule);
 
 -- GENERAL
 local General = Window:NewTab("General")
+
+local GeneralSec = General:NewSection("General")
+GeneralSec:NewButton("Claim Group Benefits", "Sends a claim group benefits event", function() 
+    game:GetService("ReplicatedStorage").Functions.ClaimGroupBenefits:InvokeServer()
+end)
+GeneralSec:NewButton("Pickup Factory", "Pickups gems from gem factory", function()
+    game:GetService("ReplicatedStorage").Events.ClaimFactoryCraft:FireServer(1)
+    game:GetService("ReplicatedStorage").Events.ClaimFactoryCraft:FireServer(2)
+    game:GetService("ReplicatedStorage").Events.ClaimFactoryCraft:FireServer(3)
+end)
+
+local Settings = General:NewSection("Settings")
+Settings:NewToggle("Debug", "Used in development", function(state)
+    getgenv().useDebug = state
+end)
+Settings:NewKeybind("Toggle UI", "Toggles UI", Enum.KeyCode.P, function()
+	Library:ToggleUI()
+end)
