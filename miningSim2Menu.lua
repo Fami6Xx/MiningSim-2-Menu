af92c9ddc86ed4bdbb155f30f2863a894ebe6820
Fami6Xx <libojer@gmail.com>
2022-11-24 19:12:20 +0100

    feat: lib & window

---
+++ miningSim2Menu.lua
@@ -0,0 +1,6 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Mining Simulator 2", "BloodTheme")
+local v8 = require(game:GetService("ReplicatedStorage").LoadModule);
+
+-- GENERAL
+local General = Window:NewTab("General")
